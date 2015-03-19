dc102ea3402e3e69b20efe82fdc86f42067eb4b6
Alan Hecht <aghecht42@gmail.com>
2015-03-19 03:34:58 +0000

    9.3 showing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def show
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
   end
   
   def edit
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def update
