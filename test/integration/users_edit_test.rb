610edd9b4977cb67faae064c865ecc02ad6b1e54
Alan Hecht <aghecht42@gmail.com>
2015-03-19 03:02:07 +0000

    9.2 authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     patch user_path(@user), user: { name: '',
                                     email: 'foo@invalid',
@@ -15,8 +16,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
   
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
